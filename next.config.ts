765f9d222d1806c4b2e6486074e6f6b80bbb3eb7
Dineth <dinethfiverr23@gmail.com>
2025-05-22 12:08:09 +0530

    Add Layout And Video Upload-Page , Do Some Change To Video Upload Api Route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb', // or larger, like '100mb'
+    }
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
